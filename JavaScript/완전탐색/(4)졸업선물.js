7923b27b9d98004cc1242d7e528479e662465d5b
multicampus <pica1217@naver.com>
2021-05-06 23:52:42 +0900

    ADD 0506

--- JavaScript/완전탐색/(4)졸업선물.js
+++ JavaScript/완전탐색/(4)졸업선물.js
@@ -1,8 +1,31 @@
 function solution(N, arr) {
-    let answer = 0,
-        stu = Array.from({ length: arr.length }, (v, idx) => idx);
+    let answer = -1;
 
-    for (let i of arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let sum = N - (Math.floor(arr[i][0] / 2) + arr[i][1]);
+        let cnt = 1;
+        let temp = arr.filter((v, index) => {
+            return index != i;
+        });
+        console.log(...temp);
+        temp = temp.sort(function (a, b) {
+            return (
+                a.reduce((curr, acc) => acc + curr, 0) -
+                b.reduce((curr, acc) => acc + curr, 0)
+            );
+        });
+        // console.log(...temp);
+        for (let i of temp) {
+            sum -= i;
+            if (sum >= 0) {
+                cnt += 1;
+            } else {
+                continue;
+            }
+        }
+        if (cnt >= answer) {
+            answer = cnt;
+        }
     }
     return answer;
 }
